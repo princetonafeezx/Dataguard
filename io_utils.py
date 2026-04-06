e414256ce535e71ccc1885da43dd737068d633dd
PrincetonAfeez <princetonafeez@gmail.com>
2026-04-06 16:32:02 -0700

    UTF-8 BOM marker & UTF-8 decoding

--- io_utils.py
+++ io_utils.py
@@ -5,4 +5,9 @@ from typing import TypedDict
 
 from dataguard.errors import InputError
 
+UTF8_BOM = b"\xef\xbb\xbf"
 
+_LATIN1_FALLBACK_WARNING = (
+    "UTF-8 decode failed; interpreted bytes as Latin-1 (ISO-8859-1); "
+    "if this is wrong, convert the input to UTF-8."
+)
